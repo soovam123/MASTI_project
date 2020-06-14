49068eb6d29ecd947e0c79a7f78a7d7efe3e2b04
abhivijay <A.Vijay-2@student.tudelft.nl>
2020-06-14 12:21:55 +0200

    Committing test results for draft report

--- p4_codel/testing_script.sh
+++ p4_codel/testing_script.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-#echo `pwd`
-mkdir -p ./testOPs 
-#sudo rm -rf out/*
+# Check for folder name here
+if [ $# -eq 0 ]
+then
+    echo "Give the folder name corresponding to the test params"
+    exit 1
+fi
+
+# Create folders
+param=$1
+mkdir -p ./testOPs/${param}
 
 bws=("16M" "14M" "12M" "10M" "8M" "6M" "4M" "2M")
 
-for i in "${bws[@]}"
+# Run tests and evals. Save to corresponding file
+for j in {0..20}
 do
-    echo "Running test for BW = $i"
-    ./simple_run.sh --nocli --iperft 10 --bw "${i}" > /dev/null 2>&1
-    ./evaluate.sh > ./testOPs/test_bw_${i}.txt 2>&1
+    for i in "${bws[@]}"
+    do
+        echo "Running test iteration $j for BW = $i"
+        ./simple_run.sh --nocli --iperft 10 --bw "${i}" > /dev/null 2>&1
+        ./evaluate.sh > ./testOPs/${param}/test_${j}_bw_${i}.txt 2>&1
+    done
 done
+
+# Play sound to denote end of script
+paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
