d2450f0bc78e4367514b3a6318df2d3aba2b2b0f
abhivijay <A.Vijay-2@student.tudelft.nl>
2020-07-10 09:26:07 +0200

    More test outputs added

--- p4_codel/testing_script.sh
+++ p4_codel/testing_script.sh
@@ -7,21 +7,29 @@ then
     exit 1
 fi
 
-# Create folders
-param=$1
-mkdir -p ./testOPs/${param}
-
-bws=("9M" "8M" "7M" "6M" "5M" "4M" "3M")
-
-# Run tests and evals. Save to corresponding file
-for j in {0..20}
+for x in {1..3}
 do
-    for i in "${bws[@]}"
-    do
-        echo "Running test iteration $j for BW = $i"
-        ./simple_run.sh --nocli --iperft 30 --bw "${i}" > /dev/null 2>&1
-        ./evaluate.sh 1> ./testOPs/${param}/test_${j}_bw_${i}.txt
-    done
+	# Move the simple_router_x to ./srcP4/simple_router.p4
+	p4_file="simple_router_${x}.p4"
+	# /home/abhi/Documents/MASTI_project/p4_codel/srcP4/simple_router.p4
+	cp ~/${p4_file} ./srcP4/simple_router.p4
+
+	# Create folder
+	param="${1}_${x}"
+	mkdir -p ./testOPs/${param}
+
+	bws=("9M" "8M" "7M" "6M" "5M" "4M" "3M")
+
+	# Run tests and evals. Save to corresponding file
+	for j in {0..19}
+	do
+	    	for i in "${bws[@]}"
+	    	do
+			echo "Running test iteration $j for BW = $i"
+			./simple_run.sh --nocli --iperft 30 --bw "${i}" > /dev/null 2>&1
+			./evaluate.sh 1> ./testOPs/${param}/test_${j}_bw_${i}.txt
+	    	done
+	done
 done
 
 # Play sound to denote end of script
